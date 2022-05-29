89f0c704352f31199ae05c8eaa5f7ec3066d4e32
zicrondev <95478413+zicrondev@users.noreply.github.com>
2022-05-29 18:32:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let button = document.querySelector(".arrow"),
+links = document.querySelector(".nav-links");
+
+button.addEventListener("click",()=>{
+    links.classList.toggle("display");
+    button.children[0].classList.toggle("toggle1")
+    button.children[0].classList.toggle("toggle2")
+    button.children[0].classList.toggle("toggle3")
+})
+
